996ca19090af3fccb6c54c264248b368d5b79d78
Chandar-uoo <chandru666v@gmail.com>
2025-07-18 15:40:07 +0530

    added state handler

---
+++ src/components/EmptyState.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EmptyState = ({message}) => {
+  return (
+    <div className='text-center py-8 text-gray-500 font-medium'>
+            {message}
+    </div>
+  )
+}
+
+export default EmptyState;

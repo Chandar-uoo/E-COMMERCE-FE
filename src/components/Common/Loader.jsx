9d9b158bfd72ce97f5c866722713aca2c66da917
Chandar-uoo <chandru666v@gmail.com>
2025-07-23 15:14:50 +0530

    feat added loading file to show loading state

---
+++ src/components/Common/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div className='flex justify-center items-center h-40 text-blue-500 font-semibold text-xl'>
+        Loading......
+    </div>
+  )
+}
+
+export default Loader;

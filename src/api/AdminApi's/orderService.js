12dcfdfe63d410777500e6dc4d1924017e384650
Chandar-uoo <chandru666@vgmail.com>
2025-07-26 22:36:15 +0530

    refactor added update status api & implemnt dynamic fetch

--- src/api/AdminApi's/orderService.js
+++ src/api/AdminApi's/orderService.js
@@ -1,7 +1,12 @@
 import axiosInstance from "../../api/axiosInstance"
 
 
-export const fetchOrders =  async()=>{
-        const res = await axiosInstance.get("/admin/orders");
+export const fetchOrders =  async(Status)=>{
+        const res = await axiosInstance.get(`/admin/orders?orderStatus=${Status}`);
         return res.data.result;
+}
+
+export const updateOrderStatus = async (orderId) => {
+    const res = await axiosInstance.patch(`/admin/update-Order-Status/${orderId}`);
+    return res.data.result;
 }

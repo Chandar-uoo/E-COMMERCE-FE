457e303a9c0f2fbaa022c8755c6a6d2f2ec492f0
Chandar-uoo <chandru666@vgmail.com>
2025-07-27 11:27:10 +0530

    style : ui updation

--- src/components/Common/Loader.jsx
+++ src/components/Common/Loader.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Loader = () => {
   return (
     <div className='flex justify-center items-center h-40 text-blue-500 font-semibold text-xl'>
-        Loading......
+        <span className="loading loading-spinner loading-xl"></span>
     </div>
   )
 }

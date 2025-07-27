3993d1f97719cc024cd728636e212ae3ba0effca
Chandar-uoo <chandru666@vgmail.com>
2025-07-27 18:24:03 +0530

    chore removed unused code & added nav to home

--- src/components/admin/Header.jsx
+++ src/components/admin/Header.jsx
@@ -1,38 +1,35 @@
-import React from 'react';
+import { useNavigate } from "react-router-dom";
+
 
 const Header = ({ onMenuClick }) => {
+ const nav =  useNavigate();
+    
   return (
     <div className="navbar bg-base-100 shadow-sm">
       <div className="navbar-start">
         <button onClick={onMenuClick} className="btn btn-ghost btn-circle">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none"
-            viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h7" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h7"
+            />
           </svg>
         </button>
       </div>
+
       <div className="navbar-center">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a onClick={()=>nav("/admin/home")} className="btn btn-ghost text-xl">A - Z commerce</a>
       </div>
-      <div className="navbar-end">
-        {/* Search & Notification */}
-        <button className="btn btn-ghost btn-circle">
-          <svg className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-          </svg>
-        </button>
-        <button className="btn btn-ghost btn-circle">
-          <div className="indicator">
-            <svg className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                d="M15 17h5l-1.4-1.4A2 2 0 0118 14.1V11a6 6 0 00-4-5.7V5a2 2 0 10-4 0v.3C7.7 6.2 6 8.4 6 11v3.2c0 .5-.2 1-.6 1.4L4 17h5m6 0v1a3 3 0 11-6 0v-1" />
-            </svg>
-            <span className="badge badge-xs badge-primary indicator-item"></span>
-          </div>
-        </button>
-      </div>
+
+      
     </div>
   );
 };

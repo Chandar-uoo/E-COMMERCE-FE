e8ae491a18ffa909a42f7bf67ea09f7029996ae0
Chandar-uoo <chandru666@vgmail.com>
2025-08-02 12:43:17 +0530

    refactor added profile& password updation and removed unused code

--- src/api/userService.js
+++ src/api/userService.js
@@ -2,31 +2,33 @@ import axiosInstance from "./axiosInstance";
 
 
 export const loginService = async (email, password) => {
-    try {
         const res = await axiosInstance.post("/auth/login", {
             email,
             password
         });
         return res.data;
-
-    } catch (err) {
-        console.log(err);
-    }
-
 }
 
 export const signupService = async (formData) => {
-    try {
         const res = await axiosInstance.post('/auth/signup', formData);
-        return res.data.result;
-
-    } catch (err) {
-        console.log(err);
-
-    }
+        return res.data;
 };
 
 export const logoutService =  async()=>{
     const res = await axiosInstance.get("/auth/logout");
     return res.data.message;
+}
+
+export const editProfileService =  async (formData) => {
+    const res =  await axiosInstance.post("/user/updateUser",{
+        name:formData.name,
+        DOB:formData.DOB,
+        image:formData.image,
+        address:formData.address,
+        phoneNo:formData.phoneNo
+    });
+    return res.data.result;
+}
+export const updatePasswordService =  async({oldPassword,newPassword})=>{
+     await axiosInstance.patch("/user/updatePassword",{oldPassword,newPassword});
 }

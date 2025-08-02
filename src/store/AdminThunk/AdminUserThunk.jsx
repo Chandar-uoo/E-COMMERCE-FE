605f9d21c4bb03f5edd87b25a25ca8e229c6d688
Chandar-uoo <chandru666@vgmail.com>
2025-08-02 13:05:22 +0530

    chore removed unused code

--- src/store/AdminThunk/AdminUserThunk.jsx
+++ src/store/AdminThunk/AdminUserThunk.jsx
@@ -1,25 +1,14 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { fetchUsers } from "../../api/AdminApi's/userService";
 
-
- export const AdminUserThunk = createAsyncThunk(
-   "user/addToCart",
-   async (_, { getState, rejectWithValue }) =>  {
-        try{
-            const response =  await fetchUsers();
-            const customers = getState().adminUserState.customers;
-            const updatedCustomers = [...customers];
-            updatedCustomers.push(...response);
-            // Optional: clean up any accidental duplicates based on `id`
-            const uniqueCustomers = Array.from(
-              new Map(updatedCustomers.map(item => [item._id, item])).values()
-            );
-            // Update the state with unique customers
-            return uniqueCustomers;
-        }
-        catch(err){
-            console.log(err);
-            return rejectWithValue(err.response.data);
-        }
-   }
- );
+export const AdminUserThunk = createAsyncThunk(
+  "user/addToCart",
+  async (user, { getState, rejectWithValue }) => {
+    try {
+      const response = await fetchUsers(user);
+      return response;
+    } catch (err) {
+      return rejectWithValue(err.message || "Something went wrong");
+    }
+  }
+);

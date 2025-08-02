d5d5fc0d7a4431903e3b4c7dea72f15cc6e6bf83
Chandar-uoo <chandru666@vgmail.com>
2025-08-02 12:59:52 +0530

    refactor updation on error sending to state

--- src/store/AdminThunk/AdminOrderThunk.jsx
+++ src/store/AdminThunk/AdminOrderThunk.jsx
@@ -1,30 +1,32 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { fetchOrders, updateOrderStatus } from "../../api/AdminApi's/orderService";
+import {
+  fetchOrders,
+  updateOrderStatus,
+} from "../../api/AdminApi's/orderService";
 
-
-export const FetchOrdersThunk =  createAsyncThunk(
-    "admin/fetchOrders",
-    async (status, { rejectWithValue }) => {
-        try {
-            const response = await fetchOrders(status);
-            return response;
-        } catch (error) {
-            return rejectWithValue(error.response.data);
-        }
+export const FetchOrdersThunk = createAsyncThunk(
+  "admin/fetchOrders",
+  async (status, { rejectWithValue }) => {
+    try {
+      const response = await fetchOrders(status);
+      return response;
+    } catch (err) {
+      return rejectWithValue(err.message || "Something went wrong");
     }
-)
+  }
+);
 export const UpdateOrderStatusThunk = createAsyncThunk(
-    "admin/updateOrderStatus",
-    async (orderId, {getState,rejectWithValue }) => {
-        try {
-            const response = await updateOrderStatus(orderId);
-            const state = getState().adminOrderState.orders;
-            const updatedOrders = state.map(order => 
-                order._id === orderId ? { ...order, status: response.status } : order
-            );  
-            return updatedOrders;
-        } catch (error) {
-            return rejectWithValue(error.response.data);
-        }
+  "admin/updateOrderStatus",
+  async (orderId, { getState, rejectWithValue }) => {
+    try {
+      const response = await updateOrderStatus(orderId);
+      const state = getState().adminOrderState.orders;
+      const updatedOrders = state.map((order) =>
+        order._id === orderId ? { ...order, status: response.status } : order
+      );
+      return updatedOrders;
+    } catch (err) {
+      return rejectWithValue(err.message || "Something went wrong");
     }
+  }
 );

c8ffb90de67cc21b88c54b20f00f139e463e53f6
Chandar-uoo <chandru666@vgmail.com>
2025-08-15 11:56:13 +0530

    feat added measure on protected route

--- src/utils/AdminRoute.jsx
+++ src/utils/AdminRoute.jsx
@@ -1,12 +1,33 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate} from "react-router-dom";
 import { useSelector } from "react-redux";
 
+import Loader from "../components/Common/Loader";
+import ErrorMessage from "../components/Common/ErrorMessage";
+;
+
+
 const AdminRoute = ({ children }) => {
-  const user = useSelector((state) => state.user.user);
-  // Check if the user is logged in and has the admin role
-  if (!user || user.role !== "admin") {
-    return <Navigate to="/unauthorized" />; // redirect if not admin
+  const {user,loading,error} = useSelector((state) => state.user);
+const nav = useNavigate();
+
+ if(error === "Session expired. Please login again."){
+     nav("/login")
   }
+  if (loading || !user) {
+    return <Loader />;
+  }
+ 
+setTimeout(() => {
+     if (!user) {
+    return <Navigate to="/login" />;
+  }
+
+  if (user.role !== "admin") {
+    return <Navigate to="/unauthorized" />;
+  }
+}, 100);
+
+ 
 
   return children; // show the admin page if allowed
 };

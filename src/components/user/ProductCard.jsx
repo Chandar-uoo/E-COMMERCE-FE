dcbe7fc9f85b6dc4e6465a88f057419d8733f4fb
Chandar-uoo <chandru666@vgmail.com>
2025-08-15 12:05:17 +0530

    chore fixed details accesing of product

--- src/components/user/ProductCard.jsx
+++ src/components/user/ProductCard.jsx
@@ -24,17 +24,16 @@ const ProductCard = ({ product }) => {
     <div className="bg-white shadow-md rounded-2xl overflow-hidden p-4 hover:shadow-xl transition duration-300 w-full sm:w-60">
       <div className="h-40 bg-gray-100 flex items-center justify-center overflow-hidden rounded-xl mb-3">
         <img onClick={() => veiwProduct(product._id)}
-          src={product.img}
-          alt={product.ProductName}
+          src={product.thumbnail}
+          alt={product.title}
           className="object-cover h-full w-full"
         />
       </div>
       <h3 className="text-md font-semibold text-gray-800 mb-1 truncate">
-        {product.ProductName}
+        {product.title}
       </h3>
-      <p className="text-sm text-gray-600 mb-1">₹{product.price}</p>
-      <p className="text-sm text-yellow-500 mb-2">⭐ {product.rating}</p>
-      <button onClick={() => dispatch(addToCart( product._id ))} className="bg-blue-600 text-white text-sm py-1 px-3 rounded hover:bg-blue-700 transition">
+      <p className="text-sm text-gray-600 mb-1">${product.price}</p>
+      <button onClick={() => dispatch(addToCart({ productId:product._id ,quantity:1}))} className="bg-blue-600 text-white text-sm py-1 px-3 rounded hover:bg-blue-700 transition">
         Add to Cart
       </button>
     </div>

b8471791bd094ffc4fd7a5878e903c8d0ba35bba
Chandar-uoo <chandru666@vgmail.com>
2025-08-28 16:38:21 +0530

    refactor improve admin route handling.

--- src/utils/AdminRoute.jsx
+++ src/utils/AdminRoute.jsx
@@ -1,33 +1,26 @@
-import { Navigate, useNavigate} from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 import Loader from "../components/Common/Loader";
 import ErrorMessage from "../components/Common/ErrorMessage";
-;
-
-
 const AdminRoute = ({ children }) => {
-  const {user,loading,error} = useSelector((state) => state.user);
-const nav = useNavigate();
+  const { user, loading, error } = useSelector((state) => state.user);
+  const nav = useNavigate();
 
- if(error === "Session expired. Please login again."){
-     nav("/login")
+  if (error === "Session expired. Please login again.") {
+    nav("/login");
   }
   if (loading || !user) {
     return <Loader />;
   }
- 
-setTimeout(() => {
-     if (!user) {
+
+  if (!user) {
     return <Navigate to="/login" />;
   }
 
   if (user.role !== "admin") {
     return <Navigate to="/unauthorized" />;
   }
-}, 100);
-
- 
 
   return children; // show the admin page if allowed
 };

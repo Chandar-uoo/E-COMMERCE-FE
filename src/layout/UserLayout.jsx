cf4d85e26673a7b20b49adf62c1f400d7f3086d2
Chandar-uoo <chandru666@vgmail.com>
2025-09-02 15:53:25 +0530

    style improved ui

--- src/layout/UserLayout.jsx
+++ src/layout/UserLayout.jsx
@@ -5,14 +5,26 @@ import NavBar from '../components/user/NavBar'
 import { Outlet } from 'react-router-dom'
 import Footer from '../components/user/Footer'
 
+
 const UserLayout = () => {
   return (
-    <div>
-      <NavBar/>
-      <Outlet/>
-      <Footer/>
-    </div>
-  )
-}
+    <div className="flex flex-col max-h-screen">
+      {/* Header always visible */}
+      <header className="sticky top-0 z-50">
+        <NavBar />
+      </header>
 
-export default UserLayout
+      {/* Content takes remaining space */}
+      <main className="flex-1">
+        <Outlet />
+      </main>
+
+      {/* Footer sticks to bottom if page is short, but also moves down on scroll */}
+      <footer className=" bottom-0">
+        <Footer />
+      </footer>
+    </div>
+  );
+};
+
+export default UserLayout;

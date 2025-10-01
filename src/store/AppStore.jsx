3f2c513301e6ae6d940d86bb359c9e7fa298664b
Chandar-uoo <chandru666@vgmail.com>
2025-10-01 18:53:59 +0530

    feat(store): refactor AppStore to integrate API reducers and enhance middleware

--- src/store/AppStore.jsx
+++ src/store/AppStore.jsx
@@ -1,21 +1,37 @@
 import { configureStore } from "@reduxjs/toolkit";
-import  productReducer from "./Slices/ProductSlice";
-import userReducer from "./Slices/UserSlice"
-import orderReducer from "./Slices/OrderSlice"
-import cartReducer from "./Slices/CartSlice"
-import adminUserReducer from "./AdminSlice/AdminUserSlice"
+import userReducer from "./Slices/UserSlice";
+import orderReducer from "./Slices/OrderSlice";
+import cartReducer from "./Slices/CartSlice";
+import adminUserReducer from "./AdminSlice/AdminUserSlice";
 import adminOrderReducer from "./AdminSlice/AdminOrderSlice";
-import adminProductReducer from "./AdminSlice/AdminProductSlice"
+import adminProductReducer from "./AdminSlice/AdminProductSlice";
+import { productApi } from "../services/user/productApi";
+import { userApi } from "../services/user/userApi";
+import { authApi } from "../services/auth/authApi";
+import { cartApi } from "../services/user/cartApi";
+import { orderApi } from "../services/user/orderApi";
+
 export const AppStore = configureStore({
-    reducer:{
-        /*user*/
-        user:userReducer,
-        products:productReducer,
-        cart:cartReducer,
-        order:orderReducer,
-        /*admin*/
-        adminUserState:adminUserReducer,
-        adminOrderState:adminOrderReducer,
-        adminProductState:adminProductReducer
-    }
-})
+  reducer: {
+    /*user*/
+    user: userReducer,
+    [productApi.reducerPath]: productApi.reducer,
+    [userApi.reducerPath]: userApi.reducer,
+    [authApi.reducerPath]: authApi.reducer,
+    [cartApi.reducerPath]: cartApi.reducer,
+    [orderApi.reducerPath]:orderApi.reducer,
+    cart: cartReducer,
+    order: orderReducer,
+    /*admin*/
+    adminUserState: adminUserReducer,
+    adminOrderState: adminOrderReducer,
+    adminProductState: adminProductReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware()
+      .concat(productApi.middleware)
+      .concat(userApi.middleware)
+      .concat(authApi.middleware)
+      .concat(cartApi.middleware)
+      .concat(orderApi.middleware),
+});

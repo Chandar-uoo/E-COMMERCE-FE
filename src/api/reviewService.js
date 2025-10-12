0c5d7c9383cf8f4c47a30f03623dcf88cab37abb
Chandar-uoo <chandru666@vgmail.com>
2025-10-12 15:55:56 +0530

    chore removed unused component

--- src/api/reviewService.js
+++
@@ -1,6 +0,0 @@
-import axiosInstance from "./axiosInstance"
-
-export const postReviewService =  async ({id,comment,rating}) => {
-    const res =  await axiosInstance.post(`/review/${id}`,{comment,rating});
-    return res.data.result;
-}

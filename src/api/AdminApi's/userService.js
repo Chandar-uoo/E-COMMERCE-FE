c3dc8cd2c9c7295e1a76b163da27caff997f1997
Chandar-uoo <chandru666@vgmail.com>
2025-10-12 15:54:35 +0530

    chore removed unused components

--- src/api/AdminApi's/userService.js
+++
@@ -1,7 +0,0 @@
-import axiosInstance from "../../api/axiosInstance"
-
-
-export const fetchUsers =  async(query)=>{
-        const res = await axiosInstance.get(`/admin/user?${query}`);
-        return res.data.result;
-}
